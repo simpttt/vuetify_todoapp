5332d239f0436afe6238f45aa0edc0d5321f1aa5
simpttt <t.takada0030@gmail.com>
2020-08-15 14:28:45 +0900

    1428_2020-08-15

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-navigation-drawer app v-model="drawer" clipped>
+      <v-container>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              Navigation list
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+      </v-container>
+    </v-navigation-drawer>
+    <v-app-bar x-large color="green" dark app clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Todoapp</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text style="text-transform: none">Login</v-btn>
+        <v-btn text style="text-transform: none">Help</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-content class="my-3">
+      <Board></Board>
+    </v-content>
+    <v-footer color="green" dark app>
+      simp_t
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+import Board from "./components/Board.vue";
+export default {
+  name: "App",
+  components: {
+    Board
+  },
+  data() {
+    return {
+      drawer: false
+    };
+  }
+};
+</script>
---
+++ src/components/Board.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Board">
+    <Task />
+  </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+export default {
+  name: 'Board',
+  components: {
+    Task
+  }
+}
+</script>
+<style></style>
